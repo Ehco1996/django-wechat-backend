c86339e0fa088e18343c8eb8874574124c3206e3
ehco1996 <zh19960202@gmail.com>
2017-06-21 22:52:20 +0800

    Models and Databases

--- exec_001/hello/admin.py
+++ exec_001/hello/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Page,Category
 
 # Register your models here.
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display=['title','category','url']
+
+
+admin.site.register(Category)
+admin.site.register(Page,PageAdmin)
--- exec_001/hello/models.py
+++ exec_001/hello/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128,unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
