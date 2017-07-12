5a37bc98521c38cf975cbdaa90f66f137ffb3b21
ehco1996 <zh19960202@gmail.com>
2017-07-12 16:24:04 +0800

    django-sspanel 使用Bulma 作为前端框架

--- django_sspanel/django_sspanel/urls.py
+++ django_sspanel/django_sspanel/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'sspanel', include('shadowsocks.urls', namespace='shadowsocks')),
+    url(r'', include('shadowsocks.urls', namespace='shadowsocks')),
 
 ]
--- django_sspanel/shadowsocks/views.py
+++ django_sspanel/shadowsocks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('sspanel index ')
+    '''跳转到首页'''
+    return render_to_response('base.html')
