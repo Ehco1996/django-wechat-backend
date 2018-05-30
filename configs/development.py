c76c84c1bdfd4121d9022421d584967d59edf40e
ehco1996 <zh19960202@gmail.com>
2018-05-30 09:26:50 +0800

    更改项目结构

---
+++ configs/development.py
@@ -0,0 +1,7 @@
+from configs.default import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    'ehco.ngrok.cc',
+    '127.0.0.1', ]
---
+++ configs/production.py
@@ -0,0 +1,7 @@
+from configs.default import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'ehco.ngrok.cc',
+    '127.0.0.1', ]
