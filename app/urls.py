74ea55e8b53545aef39f1d8c7c7dd8dfb6139813
ehco1996 <zh19960202@gmail.com>
2018-06-03 20:11:35 +0800

    fix url

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('jet/', include('jet.urls', 'jet')),
+    re_path(r'^jet/$', include('jet.urls', 'jet')),
     path('', admin.site.urls),
     path('api/wechat/', include('app.wechat.urls')),
 ]
