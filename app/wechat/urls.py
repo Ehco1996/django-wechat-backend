8652c48f6f7e2517ff722b7054632265ea7e6959
ehco1996 <zh19960202@gmail.com>
2018-07-26 09:43:04 +0800

    fix typp

--- app/wechat/urls.py
+++ app/wechat/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 
-app_name = 'wecaht'
+app_name = 'wechat'
 urlpatterns = [
     path('', views.wechat, name='wechat'),
     path('check-port/', views.check_server_api, name='server_check'),
